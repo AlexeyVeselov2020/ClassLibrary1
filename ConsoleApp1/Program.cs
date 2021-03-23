ebf2078301d64c2cffb91a57633332b812f318c2
Alexey Veselov <alexeyveselov2000@gmail.com>
2021-03-23 07:16:25 +0300

    20.03

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using QRLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string request = Console.ReadLine();
+            Console.WriteLine(Codding.EncodeRequest(request));
+        }
+    }
+}
